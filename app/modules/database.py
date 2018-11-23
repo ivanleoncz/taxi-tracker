76fffd334cba48222b2ef7d2c0f45d9d1bc144a3
ivanlmj <ivanlmj@gmail.com>
2018-11-23 01:09:57 -0600

    fixies (db uri + parenthesis for insert method)
    - adjusting method parameter

--- app/modules/database.py
+++ app/modules/database.py
@@ -1,23 +1,24 @@
 
+from app import DB_URI
 from pymongo import MongoClient
 
 class Database():
 
     def __init__(self):
         """ Configuring MongoDB client. """
-        self.client = MongoClient(serverSelectionTimeoutMS=6000)
+        self.client = MongoClient(DB_URI, serverSelectionTimeoutMS=6000)
 
 
-    def add_location(self, ride_id, taxi_id, location, query, timestamp):
+    def add_location(self, ride_id, taxi_id, address, query, timestamp):
         db = self.client.taxi.RideTracking
         try:
             record_id = db.insert({
                 "RideID":ride_id,
                 "TaxiID":taxi_id,
-                "TrackedAddress":location.get("address"),
+                "TrackedAddress":address,
                 "GeoQuery":query,
                 "TrackTime":timestamp
-            }
+            })
             self.client.close()
             return record_id
         except Exception as e:
