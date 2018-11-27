5dd8d7b04466ae4ef4b0a8a69ee50bd197109c88
ivanlmj <ivanlmj@gmail.com>
2018-11-27 11:28:22 -0600

    creating a separate module for geocoding

---
+++ app/modules/location.py
@@ -0,0 +1,17 @@
+
+from app import API_URI, API_KEY
+from json import loads
+import requests
+
+class Location():
+
+    def reverse(self, latitude, longitude):
+        """ Performs reverse geocoding, based on geographical coordinates. """
+	query = {}
+	query["lat"] = latitude
+        query["lon"] = longitude
+        query["key"] = API_KEY                                                  
+        query["format"] = "json"
+	response = requests.get(API_URI, params=query).text
+	data = loads(response)
+	return data.get("address")
